9bb9f41e057d829b3541367c53a917551cc81fe4
Tove <seleniusmoell@gmail.com>
2017-11-03 13:13:49 +0100

    Added directory calculators + interest calculation script

---
+++ calculators/interestCalc.py
@@ -0,0 +1,18 @@
+def interestCalc(investment, interest, term):
+	sum = int(investment)
+	interest = int(interest)*0.01
+	for i in range(0, int(term)):
+		sum = sum + sum*interest
+	
+	return sum
+	
+investment = input("What is the investment? ")
+interest = input("What is the yearly interest? ")
+term = input("Over how many years? ")
+
+result = interestCalc(investment, interest, term)
+
+print "The investment will grow to " + str(result)
+
+
+	
