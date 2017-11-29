d95850dff04d00f203ac772525b9b0ed41aceb9e
Tove <seleniusmoell@gmail.com>
2017-11-29 09:01:39 +0100

    cleaned it up a bit

--- header/generate_python_file.py
+++ header/generate_python_file.py
@@ -1,6 +1,7 @@
 import datetime
  
 docname=input('Document name: ') +'.py'
+description=input('File description: ') 
 author=input('Author: ')
 version=str(input('Python version: '))
 while not (version[0] == '2' or version[0] == '3'):
@@ -10,23 +11,20 @@ now = datetime.datetime.now()
 copyright = 'Copyright ' + str(now.year)
 email=input('Email: ')
 
-headerstrings = {'version':version, 'author':author, 'copyright':copyright, 'email':email}
-
-while '@' not in email:
-	email=input('That can\'t be right. Try again. \n Email:')
-
-file = open(docname, 'w')
-
-for key, value in headerstrings.items():
+if email is not "" :
+	while '@' not in email:
+		email=input('That can\'t be right. Try again. \n Email:')
 	
-	hs = ''
-	if 'version' not in key:
-		hs = '__' + key + '__' + ' = "' + value + '"\n'
-	else:
-		hs = '\n# This script is written by ' + author + ' in Python ' + value + '\n'
 	
-	file.write(hs)
-	
+headerstrings = {'author':author, 'copyright':copyright, 'email':email}
+
+with open(docname, 'w') as file:
+	file.write('# ' + description + '\n')
+	file.write('# This script is written by ' + author + ' in Python ' + version + '\n')
+
+	for key, value in headerstrings.items():
+		file.write('__' + key + '__' + ' = "' + value + '"\n')
+		
 
 
 
